1edbf0c34e8e8cfa49e472843fe043ca902fe281
Sneha Mistry <sneha.mistry@1Rintern.local>
2022-01-18 14:33:06 +0530

    sidebar component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    EmployeeFormComponent
+    EmployeeFormComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
